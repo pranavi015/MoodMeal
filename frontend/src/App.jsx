ffa78ded12d07fcbce908083543b258b541fe1f8
Pranavi Mathur <mathurpranavi06@gmail.com>
2025-12-01 16:01:48 +0530

    frontend errors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,11 @@
-// import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-
-// import Login from './pages/login';
-// import Signup from './pages/signup';
-// import Dashboard from './pages/dashboard';
-
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<Navigate to="/login" replace />} />
-//         <Route path="/login" element={<Login />} />
-//         <Route path="/signup" element={<Signup />} />
-//         <Route path="/dashboard" element={<Dashboard />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
+import Dashboard from './pages/dashboard';
 import Meals from './pages/meals';
 import Insights from './pages/insights';
 import Swaps from './pages/swaps';
 import Profile from './pages/Profile';
-import Login from './pages/login';   // use your login page
-import Signup from './pages/signup'; // use your signup page
-
+import Login from './pages/login';   
+import Signup from './pages/signup'; 
 function App() {
   return (
     <Router>
@@ -35,11 +13,11 @@ function App() {
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
         <Route path="/meals" element={<Meals />} />
         <Route path="/insights" element={<Insights />} />
         <Route path="/swaps" element={<Swaps />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/Profile" element={<Profile />} />
       </Routes>
     </Router>
   );
